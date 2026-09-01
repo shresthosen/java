9a4c30f39a8c501ba4f57e63cae72d03b433aaf7
shresthosen <shresthosen563@gmail.com>
2026-09-01 14:11:52 +0530

    fibonacci seuence

---
+++ Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1, c;
+
+        if (n <= 0) {
+            System.out.println("Invalid input");
+        
+        }
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        sc.close();
+    }
+}
